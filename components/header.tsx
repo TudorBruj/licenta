3ca9e30826c298bb1b50c0eff22779f17e3bf946
TUDOR-ALEX BRUJ <tudor.bruj@stud.ubbcluj.ro>
2024-06-10 11:05:24 +0300

    Logo and categories fixed

--- components/header.tsx
+++ components/header.tsx
@@ -6,6 +6,7 @@ import { useAppSelector } from '@/lib/hooks';
 import SideBar from './sidebar';
 import { Button } from 'primereact/button';
 import { signOut } from '@/lib/auth';
+import Image from 'next/image';
 
 const font = Big_Shoulders_Text({ subsets: ['latin'] });
 const linkStyle = 'text-main-color font-bold ' + font.className;
@@ -16,26 +17,23 @@ export default function Header() {
   return (
     <header>
       <div className='sticky m-auto flex w-full items-center justify-between px-10 py-6'>
-        <Link
-          href='/'
-          className={
-            'text-2xl font-bold uppercase text-main-color ' + font.className
-          }
-        >
-          Logo
+        <Link href='/' className='flex items-center'>
+          <Image src='/images/Hanger.png' alt='Logo' width={120} height={40} />
         </Link>
-        <div className='flex justify-center gap-4'>
-          <Link href=" /category/women's clothing" className={linkStyle}>
-            Woman
-          </Link>
-          <Link href=" /category/men's clothing" className={linkStyle}>
-            Man
-          </Link>
-          <Link href=' /category/Accessories ' className={linkStyle}>
-            Accessories
-          </Link>
+        <div className='ml-16 flex flex-1 justify-center'>
+          <div className='flex gap-8'>
+            <Link href=" /category/women's clothing" className={linkStyle}>
+              Woman
+            </Link>
+            <Link href=" /category/men's clothing" className={linkStyle}>
+              Man
+            </Link>
+            <Link href=' /category/Accessories ' className={linkStyle}>
+              Accessories
+            </Link>
+          </div>
         </div>
-        <div className='flex'>
+        <div className='flex items-center'>
           <a href='/admin'>
             <Button
               icon='pi pi-user text-main-color text-2xl'
