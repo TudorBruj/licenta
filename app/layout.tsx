45747c8b809ee86efa78aac60a14f66ba12c3386
TUDOR-ALEX BRUJ <tudor.bruj@stud.ubbcluj.ro>
2024-06-22 22:11:54 +0300

    Set up session access from client components

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,23 +4,29 @@ import StoreProvider from '@/components/store-provider';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/primereact.css';
+import { auth } from '@/auth';
+import { SessionProvider } from 'next-auth/react';
 
 export const metadata: Metadata = {
   title: 'Ecommerce',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
   return (
     <html lang='en'>
       <body>
         <StoreProvider>
-          {/* <Header /> */}
-          {children}
-          {/* <Footer /> */}
+          <SessionProvider session={session}>
+            {/* <Header /> */}
+            {children}
+            {/* <Footer /> */}
+          </SessionProvider>
         </StoreProvider>
       </body>
     </html>
